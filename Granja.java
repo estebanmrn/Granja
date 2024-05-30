9c4d7edbf092c2eb85eb7befc3d5849dc26be2ba
estebanmrn <luisestebanmc@ufps.edu.co>
2024-05-30 11:55:02 -0500

    Granja.java

--- Granja.java
+++ Granja.java
@@ -1,47 +1,46 @@
-
-/**
- * Write a description of class Granja here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import java.util.List;
+import java.util.ArrayList;
 public class Granja
 {
-    private Animal[] animales;
+    
+    private List <Animal> animales;
     private int totalAnimales, contadorGallinas, contadorCerdos, contadorVacas; 
 
     public Granja() {
-        this.animales = new Animal [10];
+        this.animales = new ArrayList<>();
         this.totalAnimales = 0;
         this.contadorGallinas = 0;
         this.contadorCerdos = 0;
         this.contadorVacas = 0;
-    }
-    
-    public void incrementarContador (Animal animal){
-        if (animal instanceof Gallina){
-            contadorGallinas++;
-        }
-        else if (animal instanceof Cerdo){
-            contadorCerdos++;
-        }
-        else if (animal instanceof Vaca){
-       contadorVacas++;
-        }
-    }
-    
-            
+    }        
         
      public void registrarAnimal (Animal animal){
-        if (totalAnimales < animales.length){
-            animales[totalAnimales++] = animal;
-            incrementarContador(animal);
+        animales.add(animal);
+        switch (animal.getTipo()){
+            case GALLINA:
+                contadorGallinas++;
+                break;
+                case VACA:
+                    contadorVacas++;
+                    break;
+                    case CERDO:
+                        contadorCerdos++;
+                        break;
         }
-        else {
-            System.out.println("No se pueden agregar más animales.");
-            
-        }
+     }
+     
+     public int getContadorGallinas() {
+        return contadorGallinas;
     }
+
+    public int getContadorVacas() {
+        return contadorVacas;
+    }
+
+    public int getContadorCerdos() {
+        return contadorCerdos;
+    }
+     
      
      public void mostrarInfo (){
          System.out.println("Animales en la granja: ");
@@ -52,14 +51,4 @@ public class Granja
          
      }
      
-     public static void main(String[] args) {
-            Granja granja = new Granja();
-            
-            granja.registrarAnimal(new Gallina ("123", "Jertudriz", 21.3, new Fecha (2021, 5, 12), "Gallina"));
-            granja.registrarAnimal(new Cerdo ("234", "Pepe", 30, new Fecha (2022, 1, 8), "Cerdo"));
-            granja.registrarAnimal(new Vaca ("1231", "Lola", 50.1, new Fecha (2023, 7, 9), "Vaca"));
-            
-            granja.mostrarInfo();
-          
-        }
 }
